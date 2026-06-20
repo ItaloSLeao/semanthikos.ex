80edc9b2947bfa8e381826cd37c32364580a3a29
ItaloSLeao <italoleao200656@gmail.com>
2026-06-20 17:48:44 -0300

    style: change app icon

--- test_live.exs
+++
@@ -1,5 +0,0 @@
-import Phoenix.ConnTest
-import Phoenix.LiveViewTest
-@endpoint EventManagerWeb.Endpoint
-
-# We cannot easily test without a valid session, so let's just observe if there are compilation errors.
